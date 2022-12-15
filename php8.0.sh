d6bd06b3348edb961b4792cb30e8c629baab8ee9
Przybyłowicz Maxwell <74791570+365cent@users.noreply.github.com>
2022-12-15 18:33:38 -0500

    Create php8.0.sh

---
+++ php8.0.sh
@@ -0,0 +1,7 @@
+#!/bin/shell
+sudo apt update 
+sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg2
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+sudo apt update 
+sudo apt install php8.0-fpm php8.0-curl
