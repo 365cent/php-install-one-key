adc9929a6a148fd2d38b033d33ce9cf63e9268d6
Przybyłowicz Maxwell <74791570+365cent@users.noreply.github.com>
2022-12-15 18:34:30 -0500

    Update php8.0.sh

--- php8.0.sh
+++ php8.0.sh
@@ -4,4 +4,4 @@ sudo apt install -y lsb-release ca-certificates apt-transport-https software-pro
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
 wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
 sudo apt update 
-sudo apt install php8.0-fpm php8.0-curl
+sudo apt install php8.0-fpm php8.0-curl -y
